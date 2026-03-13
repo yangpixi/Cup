b55cf58fb5b44a860689434aa5e4608e86d49444
yangpixi <2602340701@qq.com>
2026-03-13 15:00:49 +0800

    chore: add commonly used utils

---
+++ Cup/Utils/Logger.swift
@@ -0,0 +1,38 @@
+//
+//  Logger.swift
+//  Cup
+//
+//  Created by 空白 on 2026/3/12.
+//
+
+import OSLog
+
+struct Logger {
+    /// The unified logger at the base of this logger.
+    private let base: os.Logger
+
+    /// Creates a logger for Cup using the specified category.
+    init(category: String) {
+        self.base = os.Logger(subsystem: Constant.bundleIdentifier, category: category)
+    }
+
+    /// Logs the given informative message to the logger.
+    func info(_ message: String) {
+        base.info("\(message, privacy: .public)")
+    }
+
+    /// Logs the given debug message to the logger.
+    func debug(_ message: String) {
+        base.debug("\(message, privacy: .public)")
+    }
+
+    /// Logs the given error message to the logger.
+    func error(_ message: String) {
+        base.error("\(message, privacy: .public)")
+    }
+
+    /// Logs the given warning message to the logger.
+    func warning(_ message: String) {
+        base.warning("\(message, privacy: .public)")
+    }
+}
